7f8bc59f853b04930e5de974a9a5b0afa4aa4845
laza33salas <lazarocode@gmail.com>
2022-08-30 15:43:23 -0300

    add metod remove

--- controllers/cityController.js
+++ controllers/cityController.js
@@ -4,18 +4,76 @@ const eventController ={
     create: async(req, res) =>{
         //const {name, image, date, description, category, place, capacity, assistance, stimated, price} = req.body
         try{
-            await new Event(req.body).save()//req.body tiene que tener si o si todas las variables antes descriptas.
+            await new City(req.body).save()//req.body tiene que tener si o si todas las variables antes descriptas.
             res.status(201).json({
                 message: 'city created',
                 success: true
             })
         } catch(error) {
             res.status(400).json({
-                message: "coul't create event",
+                message: "coul't create City",
                 success: false
             })
         }
-    }
+    },
+
+    read: async(req, res) => {
+        const {id} = req.params
+        try {
+            let city = await City.findOne({_id:id})
+            if(city){
+                res.status(200).json({
+                    message: "You get one city",
+                     response: city,
+                    success: true
+                 })
+            } else {
+                res.status(404).json({
+                    message: "could't find city",
+                    success: false
+                 })
+
+            }
+          } catch(error) {
+                console.log(error)
+                res.status(400).json({
+                message: "error",
+                success: false
+            })
+      }
+    },
+
+    
+
+    remove: async(req, res) => {
+        const {id} = req.params
+        try {
+            let city = await City.findOneAndDelete({_id:id})
+            if(city){
+                res.status(200).json({
+                    message: "You delete one city",
+                     response: city,
+                    success: true
+                 })
+            } else {
+                res.status(404).json({
+                    message: "could't find city",
+                    success: false
+                 })
+
+            }
+          } catch(error) {
+                console.log(error)
+                res.status(400).json({
+                message: "error",
+                success: false
+            })
+      }
+
+    },
+
+
+    
 }
 
 module.exports = eventController
