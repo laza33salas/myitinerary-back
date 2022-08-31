1f938424f7ab8e2eeb8af120845ed7342e0b1b66
laza33salas <lazarocode@gmail.com>
2022-08-31 19:23:24 -0300

    borramos parametros que teniamos demas

--- models/City.js
+++ models/City.js
@@ -1,17 +1,12 @@
 const mongoose = require('mongoose')
 
 const schema = new mongoose.Schema({
-    name:{type: String, required: true},
-    image: {type: String, required: true},
-    date: {type: Date, required: true},
+    city:{type: String, required: true},
     description: {type: String, required: true},
-    category: {type: String, required: true},
-    place: {type: String, required: true},
-    capacity: {type: Number, required: true},
-    assistance: {type: Number, required: true},
-    stimated: {type: Number, required: true},
-    price: {type: Number, required: true}
-
+    photo: {type: String, required: true},
+    foundation: {type: Date, required: true},
+    population: {type: Number, required: true},
+  
 })
 
 
