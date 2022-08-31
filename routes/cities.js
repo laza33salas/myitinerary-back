829a0d68001057e53dc1b1ed18524728fe7b314f
laza33salas <lazarocode@gmail.com>
2022-08-31 19:24:47 -0300

    add method update and readAll

--- routes/cities.js
+++ routes/cities.js
@@ -2,15 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 //Traer el método con desestructuración.
-const {create,read,remove} = require('../controllers/cityController')//Traigo una propiedad, metodo del objeto.
+const {create,read,remove,readAll,update} = require('../controllers/cities')//Traigo una propiedad, metodo del objeto.
 
 
 
 //router.motodo('la ruta', controlador)
 router.post('/', create)
 router.get('/:id', read)
-//router.get('/:id', readAll)
+router.get('/', readAll)
 router.delete('/:id', remove)
+router.patch('/:id', update)
 
 //localhost8000/events/
 
