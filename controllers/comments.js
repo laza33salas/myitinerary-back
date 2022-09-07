08c888ed7f2e71c1a1223df9d9080c334e1a6e13
laza33salas <lazarocode@gmail.com>
2022-09-07 19:30:08 -0300

    add controllers comments

--- controllers/comments.js
+++ controllers/comments.js
@@ -8,6 +8,7 @@ const commentController={
            let comment = await new Comment(req.body).save()//req.body tiene que tener si o si todas las variables antes descriptas.
             res.status(201).json({
                 message: 'comment created',
+              
                 success: true
             }) 
         } catch(error) {
