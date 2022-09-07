13f966a8660f1b3b00e460edcfe27e4e3fed1a17
laza33salas <lazarocode@gmail.com>
2022-09-07 19:28:55 -0300

    add route put,post, delete

--- routes/itineraries.js
+++ routes/itineraries.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-const{newItinerary, readItineraries} = require('../controllers/itinerary')
+const{newItinerary, readItineraries, updateItinerary, removeItinerary} = require('../controllers/itinerary')
 
 router.post('/', newItinerary)
 router.get('/', readItineraries)
+router.put('/:id', updateItinerary)
+router.delete('/:id', removeItinerary)
 
 module.exports = router;
