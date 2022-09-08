5c8c38ab630d9c42589f559442c6e40c063afdf2
laza33salas <lazarocode@gmail.com>
2022-09-07 22:35:42 -0300

    change type comment

--- models/Comment.js
+++ models/Comment.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const schema = new mongoose.Schema({
-    comment:{type: mongoose.Types.ObjectId, ref:'comments', required: true},
+    comment:{type: String , required: true},
     user:{type: mongoose.Types.ObjectId, ref:'users'},
     itinerary:{type: mongoose.Types.ObjectId, ref:'itineraries'}
 })
