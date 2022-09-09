c7f491ead3cac460ff9f3d6b37d282d1e4b6a7de
LeonardoMedici <leomedici06@gmail.com>
2022-09-09 14:27:13 -0300

    Change of the model of users to create logins

--- models/User.js
+++ models/User.js
@@ -4,9 +4,14 @@ const schema = new mongoose.Schema({
     name:{type: String, required: true},
     lastName:{type: String, required: true},
     mail:{type: String, required: true},
-    password:{type: String, required: true},
+    password:[{type: String, required: true}], //Array para que se guarden contraseñas.
     photo:{type: String, required: true},
-    country:{type: String, required: true}
+    country:{type: String, required: true},
+    role:{Type:String, required:true},
+    from: [{Type:String, required:true}], //array con todas las formas en las que se registro el usuario.
+    logged: {type: Boolean, required: true}, //por defecto en false, y cuando se loguea sera true.
+    verified: {type: Boolean,required: true}, //por defecto en false, y cuando se verifique sera true.
+    code: {type: String, required: true} //codigo de verificacion del usuario que se "enviara" por mail.
 })
 
 const User = mongoose.model(
