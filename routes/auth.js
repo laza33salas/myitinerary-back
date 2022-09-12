a45d44d2cb4d0d0a1c2f12ec5e2a74be231f0beb
laza33salas <lazarocode@gmail.com>
2022-09-12 15:19:11 -0300

    add route signin

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-const{newUser, readUsers, signUp, verifyMail} = require('../controllers/users')
+const{newUser, readUsers, signUp, verifyMail,signIn} = require('../controllers/users')
 
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 }); 
+
 router.post('/', newUser)
 router.post('signup', signUp)
+router.post('/signin', signIn);
 router.get('/', readUsers)
 router.get('/verify/:code', verifyMail)
 
