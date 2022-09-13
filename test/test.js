6300e08687421ae88aeff6dd6474396444bd5315
laza33salas <lazarocode@gmail.com>
2022-09-13 19:55:29 -0300

    add test get

--- test/test.js
+++ test/test.js
@@ -1,7 +1,10 @@
 //instalamos y requerimos supertest  npm install supertest --save-dev
 const request = require('supertest')
 //con supertest hacemos las peticiones para testear la api
+
 const app = require('../server') //server.js
+
+const {assert} = require('chai')
 
 
 
@@ -25,30 +28,39 @@ describe('POST /cities', function() {
             population:"2" 
         })
         .expect(201, done)
-        
-        
     })
-})
 
 
 //npm install chai
 //con chai adentro de ese test verificar alguna cosa concreta a traves de una comparacion
-describe('POST /cities', function() {
-    it('Must respond with 201 status code', function(done) {
-        request(app)
-        .post('/cities')
-        .send({
-            city:"Tu vieja",
-            country:"TuViejo",
-            description: "queti",
-            photo: "123.jpg",
-            foundation:"1500", 
-            population:"2" 
-        })
-        .then(response =>{    //cuando se haya completado la peticion
+it('Must respond with the id', function(done) {
+    request(app)
+    .post('/cities')
+    .send({
+        city:"Tu vieja",
+        country:"TuViejo",
+        description: "queti",
+        photo: "123.jpg",
+        foundation:"1500", 
+        population:"2" 
+    })
+    .then(response =>{    //cuando se haya completado la peticion
+        assert.isString(response.body.id) //este id se manda de controlador cities
+        done()
+    })
+    
+  
+})
 
-        })
-        
-      
+    
+})
+
+
+describe('GET', ()=>{
+    it('should get all cities',  (done)=> { //esta vez lo hacemos con arrow function
+    request(app)
+    .get('/cities')
+    .expect(200, done)
     })
 })
+
