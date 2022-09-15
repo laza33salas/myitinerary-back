ae5f31e7253664c93087f771febe8120a4437d39
laza33salas <lazarocode@gmail.com>
2022-09-15 15:49:40 -0300

    aplicate Joi

--- controllers/cities.js
+++ controllers/cities.js
@@ -1,11 +1,74 @@
 const City = require('../models/City')
 
+const Joi = require ('joi')
+
+const validation = Joi.object({
+    city: Joi.string()
+    .required()
+    .min(4)
+    .max(20)
+    .messages({
+        'any.required': 'NAME_REQUIRED',
+        'string.empty': 'NAME_REQUIRED',
+        'string.min': 'NAME_TOO_SHORT',
+        'string.max': 'NAME_TOO_LARGE',
+    }),
+
+    country: Joi.string()
+    .required()
+    .min(4)
+    .max(20)
+    .messages({
+             'any.required': 'COUNTRY_REQUIRED',
+             'string.empty': 'COUNTRY_REQUIRED',
+             'string.min': 'COUNTRY_TOO_SHORT',
+             'string.max': 'COUNTRY_TOO_LARGE',
+         }),
+
+    photo: Joi.string()
+    .required()
+    .uri()
+    .messages({
+        'any.required': 'IMG_REQUIRED',
+        'string.empty': 'IMG_REQUIRED',
+        'string.uri':'INVALID_URL'
+    }),
+
+    description:Joi.string()
+    .required()
+    .min(20)
+    .max(250)
+    .messages({
+             'any.required': 'DESCRPTION_REQUIRED',
+             'string.empty': 'DESCRPTION_REQUIRED',
+             'string.min': 'DESCRPTION_TOO_SHORT',
+             'string.max': 'DESCRPTION_TOO_LARGE',
+         }),
+
+    population: Joi.number()
+    .required()
+    .messages({
+        'any.required': 'POPULATION_REQUIRED',
+    }),
+
+    foundation: Joi.date()
+    .required()
+    .messages({
+        'any.required': 'DATE_REQUIRED',
+        'string.empty': 'DATE_REQUIRED',
+        
+    }),
+
+})
+
 
 const eventController ={
     create: async(req, res) =>{
         //const {name, image, date, description, category, place, capacity, assistance, stimated, price} = req.body
         try{
-
+            //validacion
+            let result = await validation.validateAsync(req.body)
+           
            let city = await new City(req.body).save()//req.body tiene que tener si o si todas las variables antes descriptas.
             res.status(201).json({
                 message: 'city created',
@@ -15,7 +78,7 @@ const eventController ={
             }) 
         } catch(error) {
             res.status(400).json({
-                message: "coul't create City",
+                message: error.message,
                 success: false
             })
         }
