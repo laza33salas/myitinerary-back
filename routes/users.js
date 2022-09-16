1cf9ac90c0b4745eb86245bc45a2ba385472dec8
laza33salas <lazarocode@gmail.com>
2022-09-16 08:20:01 -0300

    add route signoout

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-const{newUser, readUsers,signUp, verifyMail,signIn,signOut} = require('../controllers/users')
+const{newUser, readUsers,signUp, verifyMail,signIn, signOut} = require('../controllers/users')
 
 
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-}); 
+//router.get('/', function(req, res, next) {
+//  res.send('respond with a resource');
+//}); 
 router.post('/', newUser)
 router.get('/', readUsers)
-
+router.post('/logout', signOut)
 router.post('/signup', signUp)
 router.post('/signin', signIn);
-router.post('/signout', signOut);
 router.get('/verify/:code', verifyMail)
 
 
