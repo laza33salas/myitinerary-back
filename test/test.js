06055eb1823c28988ec7d613cc0ebd861890a6c8
laza33salas <lazarocode@gmail.com>
2022-09-16 09:42:58 -0300

    test sigup

--- test/test.js
+++ test/test.js
@@ -15,52 +15,117 @@ const {assert} = require('chai')
 //npx mocha (para ejecutar)
 
 //primer parametro metodo y ruta que queremos testear, segundo func callback
-describe('POST /cities', function() {
-    it('Must respond with 201 status code', function(done) {
-        request(app)
-        .post('/cities')
-        .send({
-            city:"Tu vieja",
-            country:"TuViejo",
-            description: "queti",
-            photo: "123.jpg",
-            foundation:"1500", 
-            population:"2" 
-        })
-        .expect(201, done)
-    })
+// describe('POST /cities', function() {
+//     it('Must respond with 201 status code', function(done) {
+//         request(app)
+//         .post('/cities')
+//         .send({
+//             city:"Tu vieja",
+//             country:"TuViejo",
+//             description: "queti",
+//             photo: "123.jpg",
+//             foundation:"1500", 
+//             population:"2" 
+//         })
+//         .expect(201, done)
+//     })
 
 
-//npm install chai
-//con chai adentro de ese test verificar alguna cosa concreta a traves de una comparacion
-it('Must respond with the id', function(done) {
-    request(app)
-    .post('/cities')
-    .send({
-        city:"Tu vieja",
-        country:"TuViejo",
-        description: "queti",
-        photo: "123.jpg",
-        foundation:"1500", 
-        population:"2" 
-    })
-    .then(response =>{    //cuando se haya completado la peticion
-        assert.isString(response.body.id) //este id se manda de controlador cities
-        done()
-    })
+// //npm install chai
+// //con chai adentro de ese test verificar alguna cosa concreta a traves de una comparacion
+// it('Must respond with the id', function(done) {
+//     request(app)
+//     .post('/cities')
+//     .send({
+//         city:"Tu vieja",
+//         country:"TuViejo",
+//         description: "queti",
+//         photo: "123.jpg",
+//         foundation:"1500", 
+//         population:"2" 
+//     })
+//     .then(response =>{    //cuando se haya completado la peticion
+//         assert.isString(response.body.id) //este id se manda de controlador cities
+//         done()
+//     })
     
   
-})
+// })
 
     
-})
+// })
 
 
-describe('GET', ()=>{
-    it('should get all cities',  (done)=> { //esta vez lo hacemos con arrow function
-    request(app)
-    .get('/cities')
-    .expect(200, done)
+// describe('GET', ()=>{
+//     it('should get all cities',  (done)=> { //esta vez lo hacemos con arrow function
+//     request(app)
+//     .get('/cities')
+//     .expect(200, done)
+//     })
+// })
+
+
+describe("POST /users/signup", () => {
+        it("must respond with User already exists...", (done) => {
+            request(app)
+            .post("/users/signup")
+                .send({
+                    name: "nameExample",
+                    lastName: "LastExample",
+                    photo: "http://pm1.narvii.com/6498/1a02c1a91d34a9a472841d4084095facab2d6fb4_00.jpg",
+                    country: "World",
+                    email: "bestpinkoctopus@notanexample.com",
+                    password: "h31d",
+                    role: "user",
+                    from: "example"
+                })
+            .expect(200)
+            .end((err, res)=>{
+                if (err) return done(err)
+                console.log(res.body.message)
+                return done()
+            })
+        })
+        it("must respond with Invalid email", (done) => {
+            request(app)
+            .post("/users/signup")
+                .send({
+                    name: "nameExample",
+                    lastName: "LastExample",
+                    photo: "http://pm1.narvii.com/6498/1a02c1a91d34a9a472841d4084095facab2d6fb4_00.jpg",
+                    country: "World",
+                    email: "bestpinkoctopus.notanexample.com",
+                    password: "h31d",
+                    role: "user",
+                    from: "example"
+                })
+            .expect(400)
+            .end((err, res)=>{
+                if (err) return done(err)
+                return done()
+            })
+        })
+        it("must respond with invalid password", (done) => {
+            request(app)
+            .post("/users/signup")
+                .send({
+                    name: "nameExample",
+                    lastName: "LastExample",
+                    photo: "http://pm1.narvii.com/6498/1a02c1a91d34a9a472841d4084095facab2d6fb4_00.jpg",
+                    country: "World",
+                    email: "example@notanexample.com",
+                    password: "a",
+                    role: "user",
+                    from: "example"
+                })
+            .expect(400)
+            .end((err, res)=>{
+                if (err) return done(err)
+                return done()
+            })
+        })
     })
-})
 
+
+
+        
